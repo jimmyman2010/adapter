019a307155b24052742f6a072f993b78007a2f9e
Man Tran <man.tran@axonactive.vn>
2015-04-10 17:59:07 +0700

    update ssid page and landing page

--- js/app.js
+++ js/app.js
@@ -13,14 +13,37 @@ buttonArray.forEach(function(button){
     button.onclick = function(event) {
         event.preventDefault();
         if(!menu.hasClass('dropdown-show')) {
-            menu.classList.add('dropdown-show');
-            menu.classList.remove('dropdown-hide');
+            menu.addClass('dropdown-show');
+            menu.removeClass('dropdown-hide');
         } else {
-            menu.classList.remove('dropdown-show');
-            menu.classList.add('dropdown-hide');
+            menu.removeClass('dropdown-show');
+            menu.addClass('dropdown-hide');
         }
     };
 });
+
+/**
+ * show/hide the security area
+ * @param enable
+ */
+function enableSecurity(enable)
+{
+    var securityArea = document.getElementById('enable-security');
+    if(enable) {
+        securityArea.addClass('show');
+        securityArea.removeClass('hide');
+    } else {
+        securityArea.addClass('hide');
+        securityArea.removeClass('show');
+    }
+}
+
+Element.prototype.addClass = function(className) {
+    this.className = this.className + ' ' + className;
+};
+Element.prototype.removeClass = function(className) {
+    this.className = this.className.replace(className, '').trim();
+};
 
 Element.prototype.hasClass = function(className) {
     return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
